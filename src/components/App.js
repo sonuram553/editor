81ff10fc80139eb00767ac14777b7d0369c3be7b
Sonu Ram <sonuram6960@gmail.com>
2021-07-12 15:26:56 +0530

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Editor } from "./Editor";
+
+export const App = () => {
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
+};
---
+++ src/components/Editor.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useMemo, useState } from "react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
+
+export const Editor = () => {
+  return <div>Editor</div>;
+}
