89e956a20aededcdec465a09243cb69ba4ff182e
Sonu Ram <sonuram6960@gmail.com>
2021-07-13 12:16:19 +0530

    Initialise Editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,7 +1,20 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useMemo, useState } from "react";
 import { createEditor } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 
 export const Editor = () => {
-  return <div>Editor</div>;
-}
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const [value, setValue] = useState([
+    { type: "paragraph", children: [{ text: "This is a para." }] },
+  ]);
+
+  return (
+    <Slate
+      editor={editor}
+      value={value}
+      onChange={(newValue) => setValue(newValue)}
+    >
+      <Editable />
+    </Slate>
+  );
+};
