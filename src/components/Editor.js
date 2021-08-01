11130b321659a1e55f3585e3ab2a5bdcbc2e693a
Sonu Ram <sonuram6960@gmail.com>
2021-08-01 16:24:05 +0530

    Normalize paragraphs

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,11 +1,18 @@
 import React, { useMemo, useState } from "react";
-import { createEditor } from "slate";
+import { createEditor, Element, Node, Transforms } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 
 export const Editor = () => {
-  const editor = useMemo(() => withReact(createEditor()), []);
+  const editor = useMemo(() => withParagraphs(withReact(createEditor())), []);
   const [value, setValue] = useState([
-    { type: "paragraph", children: [{ text: "This is a para." }] },
+    {
+      type: "paragraph",
+      children: [
+        { text: "This is a para." },
+        { type: "paragraph", children: [{ text: "This is a para." }] },
+        { type: "paragraph", children: [{ text: "This is a para." }] },
+      ],
+    },
   ]);
 
   return (
@@ -17,4 +24,27 @@ export const Editor = () => {
       <Editable />
     </Slate>
   );
+};
+
+const withParagraphs = (editor) => {
+  const { normalizeNode } = editor;
+
+  editor.normalizeNode = (entry) => {
+    const [node, path] = entry;
+
+    // If the element is a paragraph, ensure its children are valid.
+    if (Element.isElement(node) && node.type === "paragraph") {
+      for (const [child, childPath] of Node.children(editor, path)) {
+        if (Element.isElement(child) && !editor.isInline(child)) {
+          Transforms.unwrapNodes(editor, { at: childPath });
+          return;
+        }
+      }
+    }
+
+    // Fall back to the original `normalizeNode` to enforce other constraints.
+    normalizeNode(entry);
+  };
+
+  return editor;
 };
