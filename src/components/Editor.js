485fcf3efa8047f8b5f84e2c40c84bb7b66e0b01
Sonu Ram <sonuram6960@gmail.com>
2021-08-01 15:19:24 +0530

    Add deserialize function

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,6 +1,7 @@
 import React, { useMemo, useState } from "react";
 import { createEditor, Text } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
+import { jsx } from "slate-hyperscript";
 
 export const Editor = () => {
   const editor = useMemo(() => withReact(createEditor()), []);
@@ -26,6 +27,22 @@ export const Editor = () => {
   );
 };
 
+const defaultValue = [
+  {
+    type: "paragraph",
+    children: [
+      { text: "An opening paragraph with a " },
+      {
+        type: "link",
+        url: "https://example.com",
+        children: [{ text: "link" }],
+      },
+    ],
+  },
+  { type: "quote", children: [{ text: "A wise quote." }] },
+  { type: "paragraph", children: [{ text: "A closing paragraph!" }] },
+];
+
 const serialize = (node) => {
   if (Text.isText(node)) {
     if (node.bold) {
@@ -48,18 +65,47 @@ const serialize = (node) => {
   }
 };
 
-const defaultValue = [
-  {
-    type: "paragraph",
-    children: [
-      { text: "An opening paragraph with a " },
-      {
-        type: "link",
-        url: "https://example.com",
-        children: [{ text: "link" }],
-      },
-    ],
-  },
-  { type: "quote", children: [{ text: "A wise quote." }] },
-  { type: "paragraph", children: [{ text: "A closing paragraph!" }] },
-];
+const deserialize = (el) => {
+  if (el.nodeType === 3) {
+    return el.textContent;
+  } else if (el.nodeType !== 1) {
+    return null;
+  }
+
+  let children = Array.from(el.childNodes).map(deserialize);
+  if (children.length === 0) {
+    children = [{ text: "" }];
+  }
+
+  switch (el.nodeName) {
+    case "BODY":
+      return jsx("fragment", {}, children);
+
+    case "BR":
+      return "\n";
+
+    case "BLOCKQUOTE":
+      return jsx("element", { type: "quote" }, children);
+
+    case "P":
+      return jsx("element", { type: "paragraph" }, children);
+
+    case "A":
+      return jsx(
+        "element",
+        { type: "link", url: el.getAttribute("href") },
+        children
+      );
+
+    default:
+      return el.textContent;
+  }
+};
+
+const html = `
+<p>An opening paragraph with a <a href="https://example.com">link</a> in it.</p>
+<blockquote><p>A wise quote.</p></blockquote>
+<p>A closing paragraph!</p>
+`;
+const doc = new DOMParser().parseFromString(html, "text/html");
+console.log(deserialize(doc.body));
